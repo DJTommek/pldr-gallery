144fa3316a1786db381d4c0f30bb6a836d0de254
DJTommek <tomas@palider.cz>
2020-05-05 23:34:58 +0200

    #25 Convert permissions to database: knex db generating moved from tests to standalone script

---
+++ src/tools/knex.create-demo-db.js
@@ -0,0 +1,124 @@
+require('../webserver/private/js/functions.js');
+const pathCustom = require('../libs/path.js');
+pathCustom.defineBaseDir(__dirname + '/../../..');
+console.log(BASE_DIR)
+
+const c = require('../libs/config.js');
+const knex = require('knex')(c.db.knex)
+
+const LOG = require('../libs/log.js').setup({
+	path: __dirname + '/../../data/log/',
+	catchGlobalExceptions: true,
+});
+
+const perms = require('../libs/permissions.js');
+
+// Check if config.path is set correctly
+
+module.exports.run = async function run(purgeFirst = false, insertDemoData = true) {
+	LOG.info('(Knex) Starting creating demo db...');
+	if (purgeFirst === true) {
+		await purgeData();
+	}
+	await knex.schema.createTable(c.db.table.user, function (table) {
+		table.increments('id');
+		table.string('email');
+	});
+	await knex.schema.createTable(c.db.table.group, function (table) {
+		table.increments('id');
+		table.string('name');
+	})
+	await knex.schema.createTable(c.db.table.user_group, function (table) {
+		table
+			.integer('user_id')
+			.unsigned()
+			.notNullable()
+			.references(c.db.table.user + '.id');
+		table
+			.integer('group_id')
+			.unsigned()
+			.notNullable()
+			.references(c.db.table.group + '.id');
+	})
+	await knex.schema.createTable(c.db.table.permission, function (table) {
+		table.increments('id');
+		table
+			.integer('user_id')
+			.unsigned()
+			.references(c.db.table.user + '.id');
+		table
+			.integer('group_id')
+			.unsigned()
+			.references(c.db.table.group + '.id');
+		table
+			.string('permission')
+	})
+	if (insertDemoData === true) {
+		await fillDemoData();
+	}
+}
+
+async function purgeData() {
+	LOG.info('(Knex) Purging data requested...');
+	try {
+		await knex.schema
+			.dropTable(c.db.table.permission)
+			.dropTable(c.db.table.user_group)
+			.dropTable(c.db.table.user)
+			.dropTable(c.db.table.group)
+		LOG.info('(Knex) Data purged');
+	} catch(error) {
+		console.log(error)
+	}
+	LOG.info('(Knex) Data purged');
+}
+
+async function fillDemoData() {
+	LOG.info('(Knex) Filling db with demo data...');
+	LOG.info('(Knex) DB filling with demo users...');
+	await knex.batchInsert(c.db.table.user, [
+		{id: 1, email: 'foo@bar.com'},
+		{id: 2, email: 'foo2@bar.com'},
+		{id: 3, email: 'foo3@bar.com'},
+		{id: 4, email: 'admin@mail.com'},
+	]);
+	LOG.info('(Knex) DB filled with demo users');
+	LOG.info('(Knex) DB filling with demo groups...');
+	await knex.batchInsert(c.db.table.group, [
+		{id: perms.GROUPS.ALL, name: 'all'},
+		{id: perms.GROUPS.NON_LOGGED, name: 'non-logged'},
+		{id: perms.GROUPS.LOGGED, name: 'logged'},
+		{id: 4, name: 'group-a'},
+	]);
+	LOG.info('(Knex) DB filled with demo groups');
+	LOG.info('(Knex) DB filling with user-group relations...');
+	await knex.batchInsert(c.db.table.user_group, [
+		{user_id: 2, group_id: 4},
+		{user_id: 3, group_id: 4},
+	]);
+	LOG.info('(Knex) DB filled with user-group relations');
+	LOG.info('(Knex) DB filling with permissions...');
+	await knex.batchInsert(c.db.table.permission, [
+		// users
+		{user_id: 1, permission: '/permissions/4 - visible for user1/'},
+		{user_id: 4, permission: '/'},
+		// group:
+		{group_id: 4, permission: '/permissions/secured-folder/'},
+		// group: non-logged
+		{group_id: perms.GROUPS.ALL, permission: '/permissions/header.html'},
+		{group_id: perms.GROUPS.ALL, permission: '/empty folder/'},
+		{group_id: perms.GROUPS.ALL, permission: '/files/'},
+		{group_id: perms.GROUPS.ALL, permission: '/header and footer/'},
+		{group_id: perms.GROUPS.ALL, permission: '/map from EXIF/'},
+		{group_id: perms.GROUPS.ALL, permission: '/permissions/header.html'},
+		{group_id: perms.GROUPS.ALL, permission: '/sort-test/'},
+		{group_id: perms.GROUPS.ALL, permission: '/special-characters/'},
+		{group_id: perms.GROUPS.ALL, permission: '/thumbnails/'},
+		{group_id: perms.GROUPS.ALL, permission: '/header.html'},
+		{group_id: perms.GROUPS.ALL, permission: '/footer.html'},
+	]);
+	LOG.info('(Knex) DB filled with permissions');
+	process.exit();
+}
+
+this.run(true, true);
--- test/permissions.js
+++ test/permissions.js
@@ -1,9 +1,6 @@
 require('../src/webserver/private/js/functions.js');
-const c = require('../src/libs/config.js');
 const assert = require('assert');
 const perms = require('../src/libs/permissions.js');
-const knex = require('knex')(c.knex);
-const FS = require("fs");
 
 describe('Permissions test()', function () {
 	it('Full permissions (always true)', function () {
@@ -70,99 +67,5 @@ describe('Permissions test()', function () {
 		assert.equal(perms.test(['/test/'], '/test/bla/', true), true);
 		assert.equal(perms.test(['/test/bla'], '/test/'), true);
 		assert.equal(perms.test(['/test/bla'], '/test/', true), false);
-	});
-});
-
-describe('Permissions sqlite database',  async function () {
-	// Create testing database
-	try {
-		FS.unlinkSync(c.knex.connection.filename);
-	} catch (error) {
-		console.error('Error: cant delete database file');
-	}
-	await knex.schema.createTable('user', function (table) {
-		table.increments('id');
-		table.string('email');
-	}).createTable('group', function (table) {
-		table.increments('id');
-		table.string('name');
-	}).createTable('user_group', function (table) {
-		table
-			.integer('user_id')
-			.unsigned()
-			.notNullable()
-			.references('user.id');
-		table
-			.integer('group_id')
-			.unsigned()
-			.notNullable()
-			.references('group.id');
-	}).createTable('permission', function (table) {
-		table.increments('id');
-		table
-			.integer('user_id')
-			.unsigned()
-			.references('user.id');
-		table
-			.integer('group_id')
-			.unsigned()
-			.references('group.id');
-		table
-			.string('permission')
-	}).then(function () {
-		knex.batchInsert('user', [
-			{id: 1, email: 'foo@bar.com'},
-			{id: 2, email: 'foo2@bar.com'},
-			{id: 3, email: 'foo3@bar.com'},
-			{id: 4, email: 'admin@mail.com'},
-		]).catch(function (error) {
-			console.error('Error #1: ' + error.message);
-		});
-		knex.batchInsert('group', [
-			{id: perms.GROUPS.ALL, name: 'all'},
-			{id: perms.GROUPS.NON_LOGGED, name: 'non-logged'},
-			{id: perms.GROUPS.LOGGED, name: 'logged'},
-			{id: 4, name: 'group-a'},
-		]).catch(function (error) {
-			console.error('Error #2: ' + error.message);
-		});
-	}).then(function () {
-		console.log("some random insert");
-		knex.batchInsert('user_group', [
-			{user_id: 2, group_id: 4},
-			{user_id: 3, group_id: 4},
-		]).catch(function (error) {
-			console.error('Error #3: ' + error.message);
-		});
-		knex.batchInsert('permission', [
-			// users
-			{user_id: 1, permission: '/permissions/4 - visible for user1/'},
-			{user_id: 4, permission: '/'},
-			// group:
-			{group_id: 4, permission: '/permissions/secured-folder/'},
-			// group: non-logged
-			{group_id: 1, permission: '/permissions/header.html'},
-			{group_id: 1, permission: '/empty folder/'},
-			{group_id: 1, permission: '/files/'},
-			{group_id: 1, permission: '/header and footer/'},
-			{group_id: 1, permission: '/map from EXIF/'},
-			{group_id: 1, permission: '/permissions/header.html'},
-			{group_id: 1, permission: '/sort-test/'},
-			{group_id: 1, permission: '/special-characters/'},
-			{group_id: 1, permission: '/thumbnails/'},
-			{group_id: 1, permission: '/header.html'},
-			{group_id: 1, permission: '/footer.html'}
-		]).catch(function (error) {
-			console.error('Error #4: ' + error.message);
-		});
-	}).catch(function (error) {
-		console.error('Error #5: ' + error.message);
-	});
-
-	console.log('all is done');
-
-	it('Full permissions (always true)', async function () {
-		assert.equal('a', 'b');
-		console.log('it testing');
 	});
 });
