588eeae81eb4258bcb4f86d25edbc6969c7a1140
Tomas Palider (DJTommek) <tomas@palider.cz>
2021-10-10 15:59:26 +0200

    #34 Structure index: scanStructure rewritten to use Structure repository

--- src/libs/scanStructure.js
+++ src/libs/scanStructure.js
@@ -2,12 +2,9 @@ const CONFIG = require('./config.js');
 const LOG = require('./log.js');
 const FS = require('fs');
 const pathCustom = require('./path.js');
-const PATH = require('path');
-const readline = require('readline');
-const exifParser = require('exif-parser');
 const readdirp = require('readdirp');
 const HFS = require(BASE_DIR_GET('/src/libs/helperFileSystem.js'));
-const knex = require('./database.js');
+const structureRepository = require('./repository/structure.js');
 
 async function scan(path, options = {}) {
 	options.exif = (typeof options.exif === 'boolean') ? options.exif : false;
@@ -17,13 +14,13 @@ async function scan(path, options = {}) {
 		return;
 	}
 	module.exports.scanning = true;
-	let finds = {
-		folders: [],
-		files: []
-	};
+	let items = [];
 	LOG.info('(ScanStructure) Scanning started for path "' + path + '" (options: ' + JSON.stringify(options) + ').');
 	const readDirStartHr = process.hrtime();
-	const readDirStart = new Date();
+	const scanStarted = new Date();
+
+	let filesCount = 0;
+	let foldersCount = 0;
 
 	for await (const entry of readdirp(path, {
 		type: 'files_directories',
@@ -37,54 +34,58 @@ async function scan(path, options = {}) {
 
 		try {
 			// dirent is available if alwaysStat=false
-			const item = entry.dirent || entry.stats;
+			const entryItem = entry.dirent || entry.stats;
 
 			// If processed item is symbolic link, load stats for it to get real item (file or directory).
 			// isSymbolicLink() is always false if readdirp() option alwaysStat is true
-			let realItem = item;
-			if (item.isSymbolicLink()) {
-				realItem = FS.statSync(entry.fullPath);
+			let realEntryItem = entryItem;
+			if (entryItem.isSymbolicLink()) {
+				realEntryItem = FS.statSync(entry.fullPath);
 			}
 
-			if (realItem.isFile() && entry.basename.match((new FileExtensionMapper).regexAll) === null) {
+			if (realEntryItem.isFile() && entry.basename.match((new FileExtensionMapper).regexAll) === null) {
 				continue; // file has invalid extension
 			}
 
 			let entryPath = pathCustom.absoluteToRelative(entry.fullPath, CONFIG.path);
 
-			let pathData = {
-				path: entryPath,
-				text: entryPath,
-				created: realItem.ctimeMs || null,
-				scanned: new Date(),
-			};
-
-			if (realItem.isDirectory()) {
-				pathData.path += '/';
-				pathData.text += '/';
-				finds.folders.push(pathData);
-			} else if (realItem.isFile()) {
-				pathData.size = realItem.size || null;
+			let resultItem = null;
+			if (realEntryItem.isDirectory()) {
+				resultItem = new FolderItem();
+				foldersCount++;
+			} else if (realEntryItem.isFile()) {
+				resultItem = new FileItem();
+				resultItem.size = realEntryItem.size || null;
 				if (options.exif) {
-					pathData = Object.assign(pathData, getCoordsFromExifFromFile(entry.fullPath));
+					resultItem = Object.assign(resultItem, getCoordsFromExifFromFile(entry.fullPath));
 				}
-				finds.files.push(pathData);
+				filesCount++;
 			} else {
 				LOG.warning('Unhandled type of file, full path: "' + entry.fullPath + '"');
 				continue;
 			}
+			resultItem.path = entryPath;
+			resultItem.text = entryPath;
+			resultItem.created = realEntryItem.ctimeMs || null;
+			resultItem.scanned = new Date();
 
-			if (finds.files.length > 0 && finds.files.length % 1000 === 0) {
+			if (resultItem instanceof FolderItem) {
+				resultItem.path += '/';
+				resultItem.text += '/';
+			}
+			items.push(resultItem);
+
+			if (items.length > 0 && items.length % 1000 === 0) {
 				let humanTime = msToHuman(hrtime(process.hrtime(readDirStartHr)));
-				LOG.debug('(ScanStructure) So far scanned ' + finds.files.length + ' files and ' + finds.folders.length + ' folders in ' + humanTime + '.')
+				LOG.debug('(ScanStructure) So far scanned ' + filesCount + ' files and ' + foldersCount + ' folders in ' + humanTime + '.')
 			}
 		} catch (error) {
 			LOG.error('(ScanStructure) Scanning throwed error while processing readdirp results: ' + error);
 		}
 	}
 	let humanTime = msToHuman(hrtime(process.hrtime(readDirStartHr)));
-	LOG.info('(ScanStructure) Scanning is done in ' + humanTime + ', founded ' + finds.folders.length + ' folders and ' + finds.files.length + ' files.');
-	await updateDatabase(finds, readDirStart);
+	LOG.info('(ScanStructure) Scanning is done. Founded ' + filesCount + ' files and ' + foldersCount + ' folders in ' + humanTime + '.');
+	await structureRepository.updateData(items, scanStarted)
 	module.exports.scanning = false;
 }
 
@@ -103,67 +104,6 @@ function getCoordsFromExifFromFile(fullPath) {
 		}
 	}
 	return {}
-}
-
-/**
- * @param finds
- * @param {Date} scanStart
- */
-async function updateDatabase(finds, scanStart) {
-	const findsFolders = finds.folders.map(function (pathData) {
-		const result = {
-			path: pathData.path,
-			type: 0,
-			scanned: pathData.scanned.getTime(),
-		};
-		if (pathData.created) {
-			result.created = pathData.created;
-		}
-		return result;
-	});
-	const findsFiles = finds.files.map(function (pathData) {
-		const result = {
-			path: pathData.path,
-			type: 1,
-			scanned: pathData.scanned.getTime(),
-		};
-		if (pathData.size) {
-			result.size = pathData.size;
-		}
-		if (pathData.created) {
-			result.created = pathData.created;
-		}
-		if (typeof pathData.coordLat === 'number' && typeof pathData.coordLon === 'number') {
-			result.coordinate_lat = pathData.coordLat;
-			result.coordinate_lon = pathData.coordLon;
-		}
-		return result;
-	});
-	try {
-		const BATCH_SIZE = 10000;
-		await knex.transaction(async function (transaction) {
-			const chunk = [];
-			let chunkCount = 0;
-			const allItems = findsFiles.concat(findsFolders);
-			LOG.debug('Loaded ' + allItems.length + ' items total, updating database...')
-			for (const row of allItems) {
-				chunk.push(row);
-				if (chunk.length === BATCH_SIZE) {
-					LOG.debug('Inserting batch #' + (chunkCount++) + ' to database, ' + (allItems.length - (BATCH_SIZE * chunkCount)) + ' rows remaining.')
-					await transaction(CONFIG.db.table.structure).insert(chunk).onConflict('path').merge();
-					chunk.length = 0;
-				}
-			}
-			if (chunk.length > 0) {
-				LOG.debug('Inserting final batch #' + (chunkCount++) + ' of ' + chunk.length + ' rows to database, no rows remaining.')
-				await transaction(CONFIG.db.table.structure).insert(chunk).onConflict('path').merge();
-			}
-			const deleted = await transaction(CONFIG.db.table.structure).delete().where('scanned', '<', scanStart.getTime());
-			LOG.info('(ScanStructure) Scanned structure in database was updated (' + deleted + ' deleted).');
-		});
-	} catch (error) {
-		LOG.error('Error while inserting scanned structure to database: ' + error.message);
-	}
 }
 
 module.exports.scan = scan;
