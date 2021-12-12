3839aac56c152705a0125fad4e4b4b454954d6da
Tomas Palider (DJTommek) <tomas@palider.cz>
2021-12-12 13:12:09 +0100

    #55 Advanced search: structure.search() has now support for sorting by distance to given point

--- src/webserver/pages/api/search.js
+++ src/webserver/pages/api/search.js
@@ -12,49 +12,52 @@ module.exports = function (webserver, endpoint) {
 	 * @param path - where to search
 	 */
 	webserver.get(endpoint, async function (req, res) {
-		res.statusCode = 200;
-		res.setHeader("Content-Type", "application/json");
-		let finds = {
-			folders: [{ // always show "close searching" button
-				path: res.locals.path,
-				noFilter: true,
-				text: 'Zavřít vyhledávání "' + req.query.query + '"',
-				icon: (new Icon).CLOSE_SEARCHING,
-			}],
-			files: []
-		};
 		try {
-			if (!req.query.query) {
-				throw new Error('no search query');
-			}
+			res.statusCode = 200;
+			res.setHeader("Content-Type", "application/json");
+			let finds = {
+				folders: [{ // always show "close searching" button
+					path: res.locals.path,
+					noFilter: true,
+					text: 'Zavřít vyhledávání',
+					icon: (new Icon).CLOSE_SEARCHING,
+				}],
+				files: []
+			};
 			if (!res.locals.fullPathFolder) {
 				throw new Error('no path');
 			}
-		} catch (error) {
-			res.result.setError('Error while searching: ' + error.message).end();
-			return;
-		}
 
-		let logPrefix = '(Web) Searching "' + req.query.query + '" in path "' + res.locals.path + '"';
-		let readDirStart = process.hrtime();
-
-		res.startTime('apisearching', 'Searching');
-		(await structureRepository.search(res.locals.path, req.query.query)).forEach(function (item) {
-			if (finds.folders.length < 1000 || finds.folders.length < 1000) {
-				if(res.locals.user.testPathPermission(item.path) === false) {
-					return;	// filter out items, that user don't have permission on
-				}
-				item.text = item.path;
-				if (item.isFolder) {
-					finds.folders.push(item.serialize());
-				} else if (item.isFile) {
-					finds.files.push(item.serialize());
-				}
+			const searchOptions = {
+				searchString: req.query.query,
+				lat: req.query.lat,
+				lon: req.query.lon,
 			}
-		});
-		res.endTime('apisearching');
-		let humanTime = msToHuman(hrtime(process.hrtime(readDirStart)));
-		LOG.info(logPrefix + ' is done in ' + humanTime + ', founded ' + finds.folders.length + ' folders and ' + finds.files.length + ' files.');
-		res.result.setResult(finds, 'Done in ' + humanTime).end();
+
+			let logPrefix = '(Web) Searching in path "' + res.locals.path + '"';
+			res.startTime('apisearching', 'Searching');
+			let searchingStart = process.hrtime();
+
+			(await structureRepository.search(res.locals.path, searchOptions)).forEach(function (item) {
+				if (finds.folders.length < 1000 && finds.files.length < 1000) {
+					if (res.locals.user.testPathPermission(item.path) === false) {
+						return;	// filter out items, that user don't have permission on
+					}
+					item.text = item.path;
+					if (item.isFolder) {
+						finds.folders.push(item.serialize());
+					} else if (item.isFile) {
+						finds.files.push(item.serialize());
+					}
+				}
+			})
+			res.endTime('apisearching');
+			let humanTime = msToHuman(hrtime(process.hrtime(searchingStart)));
+			LOG.info(logPrefix + ' is done in ' + humanTime + ', founded ' + finds.folders.length + ' folders and ' + finds.files.length + ' files.');
+			res.result.setResult(finds, 'Done in ' + humanTime).end();
+		} catch (error) {
+			LOG.error(logPrefix + ' is done in ' + humanTime + ', founded ' + finds.folders.length + ' folders and ' + finds.files.length + ' files.');
+			res.result.setError('Error while searching: ' + error.message).end();
+		}
 	});
 };
