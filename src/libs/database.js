5f2556ee0ce4870a84d18c08ff43a78bceaa0d7d
Tomas Palider (DJTommek) <tomas@palider.cz>
2021-12-14 19:53:18 +0100

    Knex: added automatic logging of SQL queries including timing

--- src/libs/database.js
+++ src/libs/database.js
@@ -1,4 +1,19 @@
 const CONFIG = require('./config.js');
 const knex = require('knex')(CONFIG.db.knex);
+const LOG = require('./log.js');
+
+const queries = {};
+
+knex.on('query', function (query) {
+	const uid = query.__knexQueryUid;
+	query.timeStart = process.hrtime();
+	queries[uid] = query;
+}).on('query-response', function (response, query) {
+	const uid = query.__knexQueryUid;
+	const elapsedMs = hrtime(process.hrtime(queries[uid].timeStart));
+	LOG.debug('(Knex) Query "' + query.sql + '" with ' + query.bindings.length +  ' binding parameters [' + query.bindings.join(', ') + '] took ' + msToHuman(elapsedMs) + ' and returned ' + response.length + ' rows.', {console: true});
+	delete queries[uid];
+});
+
 module.exports = knex;
 
