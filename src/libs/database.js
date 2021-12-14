09a92021daf206ac3eb26776325fd011e3725a97
Tomas Palider (DJTommek) <tomas@palider.cz>
2021-12-14 22:20:12 +0100

    Knex: limited length of logged SQL queries

--- src/libs/database.js
+++ src/libs/database.js
@@ -11,7 +11,19 @@ knex.on('query', function (query) {
 }).on('query-response', function (response, query) {
 	const uid = query.__knexQueryUid;
 	const elapsedMs = hrtime(process.hrtime(queries[uid].timeStart));
-	LOG.debug('(Knex) Query "' + query.sql + '" with ' + query.bindings.length +  ' binding parameters [' + query.bindings.join(', ') + '] took ' + msToHuman(elapsedMs) + ' and returned ' + response.length + ' rows.', {console: true});
+
+	let sqlText = query.sql;
+	if (sqlText.length > 500) {
+		sqlText = sqlText.substring(0, 500) + '...';
+	}
+
+	let bindingsText;
+	if (query.bindings.length > 10) {
+		bindingsText = query.bindings.slice(0, 10).join(', ') + ', and more...';
+	} else {
+		bindingsText = query.bindings.join(', ');
+	}
+	LOG.debug('(Knex) Query "' + sqlText + '" with ' + query.bindings.length +  ' binding parameters [' + bindingsText + '] took ' + msToHuman(elapsedMs) + ' and returned ' + response.length + ' rows.', {console: true});
 	delete queries[uid];
 });
 
