cbe192aa4645f7122c92fabccba1606b5d09adaa
Tomas Palider (DJTommek) <tomas@palider.cz>
2022-02-08 21:24:34 +0100

    Search file size: added Utils.clamp()

--- src/libs/utils/utils.js
+++ src/libs/utils/utils.js
@@ -9,3 +9,44 @@ module.exports.requestingCmdHelp = function (arguments) {
 	const conditions = ['-h', '--h', '-help', '--help'];
 	return conditions.some(condition => argumentsLower.includes(condition));
 }
+
+
+/**
+ * Clamp (min-max) number between two numbers
+ *
+ * @param {number} input Input number to be checked
+ * @param {?number} min Number must be equal or higher than this parameter (default 0) Set null to not limit
+ * @param {?number} max Number must be equal or smaller than this parameter (default null) Set null to not limit
+ * @returns {number}
+ * @throws {Error}
+ */
+module.exports.clamp = function (input, min = 0, max = null) {
+	if (typeof input !== 'number') {
+		throw new Error('Invalid argument "input": expected "number" but got "' + typeof input + '"');
+	}
+
+	if (min === null && max === null) {
+		return input; // no need to do anything
+	}
+
+	if (typeof min === 'number') {
+		input = Math.max(input, min)
+	} else if (min === null) {
+		// valid, but do nothing
+	} else {
+		throw new Error('Invalid argument "min": expected "null" or "number" but got "' + typeof min + '"');
+	}
+
+	if (typeof max === 'number') {
+		if (max < min) {
+			throw new Error('Invalid order of arguments: "max" must be bigger or equal to "min".');
+		}
+		input = Math.min(input, max)
+	} else if (max === null) {
+		// valid, but do nothing
+	} else {
+		throw new Error('Invalid argument "max": expected "null" or "number" but got "' + typeof min + '"');
+	}
+
+	return input;
+}
