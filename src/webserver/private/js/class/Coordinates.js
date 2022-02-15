d9490518f00ba2dc7464ead8df34e3417b6ddd4d
Tomas Palider (DJTommek) <tomas@palider.cz>
2022-02-15 23:57:24 +0100

    Coordinates: used encapsulation, added json(), added safe creating

--- src/webserver/private/js/class/Coordinates.js
+++ src/webserver/private/js/class/Coordinates.js
@@ -4,18 +4,57 @@ class Coordinates {
 	 * @param {number|string} lon Longitude
 	 */
 	constructor(lat, lon) {
-		if (Coordinates.isLat(lat) === false) {
-			throw new Error('Latitude coordinate must be numeric between or equal from -90 to 90 degrees.');
-		}
-		if (Coordinates.isLon(lon) === false) {
-			throw new Error('Longitude coordinate must be numeric between or equal from -180 to 180 degrees.');
-		}
-		this.lat = parseFloat(lat);
-		this.lon = parseFloat(lon);
+		this.lat = lat;
+		this.lon = lon;
 	}
 
+	/** @returns {number} Get latitude */
+	get lat() {
+		return this._lat;
+	}
+
+	/** @returns {number} Get longitude */
+	get lon() {
+		return this._lon;
+	}
+
+	/** @param {number|string} lat Set latitude */
+	set lat(lat) {
+		if (Coordinates.isLat(lat)) {
+			this._lat = parseFloat(lat);
+		} else {
+			throw new Error('Latitude coordinate must be numeric between or equal from -90 to 90 degrees.');
+		}
+	}
+
+	/** @param {number|string} lon Set longitude */
+	set lon(lon) {
+		if (Coordinates.isLon(lon)) {
+			this._lon = parseFloat(lon);
+		} else {
+			throw new Error('Longitude coordinate must be numeric between or equal from -180 to 180 degrees.');
+		}
+	}
+
+	/** @returns {string} */
 	toString() {
-		return this.lat + ',' + this.lon;
+		return this._lat + ',' + this._lon;
+	}
+
+	/**
+	 * Return coordinates as JSON.
+	 *
+	 * @example Output can be used as LatLng parameter to LeafletJS: https://leafletjs.com/reference.html#latlng
+	 * const coords = new Coordinates(50.1, 14.4);
+	 * map.panTo(coords.json());
+	 *
+	 * @returns {{lon: number, lat: number}}
+	 **/
+	json() {
+		return {
+			lat: this._lat,
+			lon: this._lon,
+		}
 	}
 
 	/**
@@ -35,7 +74,7 @@ class Coordinates {
 	}
 
 	/**
-	 * Check if input is valid latitude
+	 * Check if input is valid longitude
 	 *
 	 * @param {number|string} lon
 	 * @returns {boolean}
@@ -48,6 +87,21 @@ class Coordinates {
 			return false;
 		}
 		return lonReal >= -180 && lonReal <= 180;
+	}
+
+	/**
+	 * Create new instance but return null if lat and/or lon are invalid
+	 *
+	 * @param {number|string} lat Latitude
+	 * @param {number|string} lon Longitude
+	 * @returns {Coordinates|null}
+	 */
+	static safe(lat, lon) {
+		try {
+			return new Coordinates(lat, lon)
+		} catch (error) {
+			return null;
+		}
 	}
 }
 
