24244bf8f2981a0238ad30b62d4b5b392aaba349
Tomas Palider (DJTommek) <tomas@palider.cz>
2022-03-06 22:34:44 +0100

    #96 Leaflet: refactored to remove duplicated code from both maps into abstract

--- src/webserver/private/js/class/StructureMap.js
+++ src/webserver/private/js/class/StructureMap.js
@@ -1,9 +1,7 @@
 /*!
  * Structure map
  */
-class StructureMap {
-
-	defaultCoords = new Coordinates(49.6, 15.2); // Czechia
+class StructureMap extends AbstractMap {
 	defaultZoom = 13;
 	defaultIcon = L.icon({
 		iconUrl: 'images/marker-photo.svg',
@@ -11,40 +9,8 @@ class StructureMap {
 		iconAnchor: [5, 5],
 	})
 
-	constructor(elementId) {
-		this.elementId = elementId;
-		this.element = null; // filled upon init
-		this.map = null;
-		this.markers = {};
-
-		this.tileLayers = {
-			'OSM default': L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-				attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/" target="_blank">Mapbox</a>',
-				maxZoom: 22,
-				minZoom: 1,
-				id: 'mapbox/streets-v11',
-				tileSize: 512,
-				zoomOffset: -1,
-				// @TODO change access_token (this is taken from from example on https://leafletjs.com/examples/quick-start/)
-				accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
-			}),
-			'Mapy.cz hiking': L.tileLayer('https://mapserver.mapy.cz/turist-m/{z}-{x}-{y}', {
-				minZoom: 2,
-				maxZoom: 18,
-				attribution: '<a href="https://o.seznam.cz" target="_blank" rel="noopener">Seznam.cz, a.s.</a>',
-				tileSize: 256,
-				zoomOffset: 0,
-			}),
-		};
-	}
-
 	init() {
-		this.element = document.getElementById(this.elementId);
-		this.map = L.map(this.element, {
-			fullscreenControl: true,
-		}).setView(this.defaultCoords, this.defaultZoom);
-		L.control.layers(this.tileLayers).addTo(this.map);
-		this.tileLayers['OSM default'].addTo(this.map); // Default layer to be displayed on page load
+		super.init();
 
 		const locateControl = L.control.locate({
 			setView: false,
@@ -118,10 +84,6 @@ class StructureMap {
 		return this.getMarker(btoa(item.coords + item.path));
 	}
 
-	getMarker(uniqueId) {
-		return this.markers[uniqueId];
-	}
-
 	addMarker(uniqueId, coords, title = null, popupContent = null) {
 		if (uniqueId in this.markers) {
 			throw new Error('Marker with ID already exists (coords: ' + coords + ')');
@@ -147,13 +109,5 @@ class StructureMap {
 			this.markers[markerId].remove();
 			delete this.markers[markerId];
 		}
-	}
-
-	mapShow() {
-		this.element.style.display = 'block';
-		this.map.invalidateSize();
-	}
-	mapHide() {
-		this.element.style.display = 'none';
 	}
 }
