3d7f367d0f7bd8d6c950dd5ecb9f8cb413c6f5e3
Tomas Palider (DJTommek) <tomas@palider.cz>
2022-04-16 20:07:54 +0200

    Thumbnails: added catching errors in thumbnail-folder

--- src/webserver/pages/api/thumbnail-folder.js
+++ src/webserver/pages/api/thumbnail-folder.js
@@ -12,89 +12,94 @@ module.exports = async function (webserver, endpoint) {
 
 	webserver.get(endpoint, async function (req, res) {
 		res.statusCode = 200;
-		if (c.thumbnails.folder.enabled !== true) {
-			// return res.result.setError('Folder thumbnail images are disabled in server config. Check out "config.thumbnails.folder.enabled".').end(403);
-			return sendTransparentPixel();
-		}
-		if (!res.locals.fullPathFolder) {
-			// return res.result.setError('Invalid path or you dont have a permission.').end(403);
-			return sendTransparentPixel();
-		}
-
-		const thumbnailPath = preparedThumbnailPath();
-		if (thumbnailPath) {
-			setHttpHeadersFromConfig();
-			res.setHeader('Content-Type', HFS.detectMimeType(thumbnailPath));
-			res.sendFile(thumbnailPath);
-			return;
-		}
-
-		// User has to have full access to checked folder to use cached files. Otherwise he would be able to see thumbnail
-		// generated from images, that are not allowed. Possible bad use case:
-		// User1 with full access generate thumbnail from images 1, 2, 3, 4 and save it to cache
-		// User2 with access to images 1, 2 is able to see thumbnail generated from images 1, 2, 3, 4 because it was cached before
-		const canUseCache = perms.test(res.locals.user.getPermissions(), res.locals.path, true);
-
-		const dispositionHeader = 'inline; filename="' + encodeURI(
-			'directory thumbnail - ' + res.locals.fullPathFolder.split('/').at(-2) + '.png'
-		) + '"';
-
-		// Use cached file if possible
-		const cacheFilePath = cacheHelper.getPath(cacheHelper.TYPE.FOLDER, res.locals.path, true);
-		if (canUseCache && c.thumbnails.folder.cache === true && cacheFilePath) {
-			setHttpHeadersFromConfig();
-			res.setHeader('Content-Type', 'image/png');
-			res.setHeader('Content-Disposition', dispositionHeader);
-			res.startTime('apicache', 'Loading cached thumbnail');
-			res.sendFile(cacheFilePath);
-			return;
-		}
-
-		// load all image files from folder
-		res.startTime('apiload', 'Loading random files');
-		const randomFiles = await structureRepository.randomFiles(res.locals.path);
-		res.endTime('apiload');
-		const randomFilesReal = randomFiles.filter(function (item) {
-			return item.isImage && perms.test(res.locals.user.getPermissions(), item.path);
-		});
-
-		if (randomFilesReal.length < c.thumbnails.folder.positions.length) {
-			return sendTransparentPixel();
-		}
-
-		// get random X images and generate promises for final composing
-		let resizePromises = [];
-		randomFilesReal.slice(0, c.thumbnails.folder.positions.length).forEach(function (folderItem, index) {
-			resizePromises.push(sharp(pathCustom.join(c.path, folderItem.path)).resize({
-				width: c.thumbnails.folder.positions[index].width,
-				height: c.thumbnails.folder.positions[index].height,
-			}).toBuffer());
-		});
-
-		res.startTime('apiresize', 'Resizing images');
-		// resize all loaded images and use them to build final thumbnail
-		Promise.all(resizePromises).then(function (resizedImages) {
-			res.endTime('apiresize');
-			res.startTime('apicompose', 'Composing thumbnail');
-			let toComposite = [];
-			resizedImages.forEach(function (resizedImage, index) {
-				toComposite.push({input: resizedImages[index], gravity: c.thumbnails.folder.positions[index].gravity})
-			});
-			const thumbnailImageStream = sharp(c.thumbnails.folder.inputOptions).composite(toComposite).png();
-
-			setHttpHeadersFromConfig();
-			res.setHeader('Content-Type', 'image/png');
-			res.setHeader('Content-Disposition', dispositionHeader);
-			thumbnailImageStream.pipe(res);
-
-			// if caching is enabled, save it
-			if (canUseCache && c.thumbnails.folder.cache === true) {
-				cacheHelper.saveStream(cacheHelper.TYPE.FOLDER, res.locals.path, thumbnailImageStream);
+		try {
+			if (c.thumbnails.folder.enabled !== true) {
+				// return res.result.setError('Folder thumbnail images are disabled in server config. Check out "config.thumbnails.folder.enabled".').end(403);
+				return sendTransparentPixel();
 			}
-		}).catch(function (error) {
-			LOG.error('Error while generating thumbnail image for folder "' + res.locals.path + '": ' + error.message);
-			return res.result.setError('Error while generating folder thumbnail image.').end(500);
-		});
+			if (!res.locals.fullPathFolder) {
+				// return res.result.setError('Invalid path or you dont have a permission.').end(403);
+				return sendTransparentPixel();
+			}
+
+			const thumbnailPath = preparedThumbnailPath();
+			if (thumbnailPath) {
+				setHttpHeadersFromConfig();
+				res.setHeader('Content-Type', HFS.detectMimeType(thumbnailPath));
+				res.sendFile(thumbnailPath);
+				return;
+			}
+
+			// User has to have full access to checked folder to use cached files. Otherwise he would be able to see thumbnail
+			// generated from images, that are not allowed. Possible bad use case:
+			// User1 with full access generate thumbnail from images 1, 2, 3, 4 and save it to cache
+			// User2 with access to images 1, 2 is able to see thumbnail generated from images 1, 2, 3, 4 because it was cached before
+			const canUseCache = perms.test(res.locals.user.getPermissions(), res.locals.path, true);
+
+			const dispositionHeader = 'inline; filename="' + encodeURI(
+				'directory thumbnail - ' + res.locals.fullPathFolder.split('/').at(-2) + '.png'
+			) + '"';
+
+			// Use cached file if possible
+			const cacheFilePath = cacheHelper.getPath(cacheHelper.TYPE.FOLDER, res.locals.path, true);
+			if (canUseCache && c.thumbnails.folder.cache === true && cacheFilePath) {
+				setHttpHeadersFromConfig();
+				res.setHeader('Content-Type', 'image/png');
+				res.setHeader('Content-Disposition', dispositionHeader);
+				res.startTime('apicache', 'Loading cached thumbnail');
+				res.sendFile(cacheFilePath);
+				return;
+			}
+
+			// load all image files from folder
+			res.startTime('apiload', 'Loading random files');
+			const randomFiles = await structureRepository.randomFiles(res.locals.path);
+			res.endTime('apiload');
+			const randomFilesReal = randomFiles.filter(function (item) {
+				return item.isImage && perms.test(res.locals.user.getPermissions(), item.path);
+			});
+
+			if (randomFilesReal.length < c.thumbnails.folder.positions.length) {
+				return sendTransparentPixel();
+			}
+
+			// get random X images and generate promises for final composing
+			let resizePromises = [];
+			randomFilesReal.slice(0, c.thumbnails.folder.positions.length).forEach(function (folderItem, index) {
+				resizePromises.push(sharp(pathCustom.join(c.path, folderItem.path)).resize({
+					width: c.thumbnails.folder.positions[index].width,
+					height: c.thumbnails.folder.positions[index].height,
+				}).toBuffer());
+			});
+
+			res.startTime('apiresize', 'Resizing images');
+			// resize all loaded images and use them to build final thumbnail
+			Promise.all(resizePromises).then(function (resizedImages) {
+				res.endTime('apiresize');
+				res.startTime('apicompose', 'Composing thumbnail');
+				let toComposite = [];
+				resizedImages.forEach(function (resizedImage, index) {
+					toComposite.push({input: resizedImages[index], gravity: c.thumbnails.folder.positions[index].gravity})
+				});
+				const thumbnailImageStream = sharp(c.thumbnails.folder.inputOptions).composite(toComposite).png();
+
+				setHttpHeadersFromConfig();
+				res.setHeader('Content-Type', 'image/png');
+				res.setHeader('Content-Disposition', dispositionHeader);
+				thumbnailImageStream.pipe(res);
+
+				// if caching is enabled, save it
+				if (canUseCache && c.thumbnails.folder.cache === true) {
+					cacheHelper.saveStream(cacheHelper.TYPE.FOLDER, res.locals.path, thumbnailImageStream);
+				}
+			}).catch(function (error) {
+				LOG.error('Error while generating thumbnail image for directory "' + res.locals.path + '": ' + error.message);
+				return res.result.setError('Error while generating directory thumbnail image.').end(500);
+			});
+		} catch (error) {
+			LOG.error('Error while preparing images for thumbnail image for directory "' + res.locals.path + '": ' + error.message);
+			return res.result.setError('Error while preparing images for directory thumbnail image.').end(500);
+		}
 
 		function setHttpHeadersFromConfig() {
 			c.thumbnails.folder.httpHeaders.forEach(function (header) {
