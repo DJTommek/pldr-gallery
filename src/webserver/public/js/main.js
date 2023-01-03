6c5b08a2f4856a6694e3c7cd43cd9c4af525442a
Tomas Palider (DJTommek) <tomas@palider.cz>
2023-01-03 20:39:46 +0100

    #110 UI and UX redesign: small rows and big rows - CSS refactored into display-flex (fixing ugly long text wrappings, replaced with ellipsis), removed some obsolete info, location icon is shown always when coordinates are available, big rows are 1em bigger, overall CSS simplification

--- src/webserver/public/js/main.js
+++ src/webserver/public/js/main.js
@@ -1205,22 +1205,19 @@ function parseStructure(items) {
 			contentTiles += ' <img class="thumbnail" src="' + transparentPixelBase64 + '">'; // fake thumbnail for proper display
 		}
 		contentTiles += ' <a href="#' + item.url + '" class="name">' + item.text + '</a>';
-		if (item.created) {
-			const created = item.created.human(true);
-			contentTiles += ' <span class="created" title="' + created + ' (' + msToHuman(Math.max(new Date().getTime() - item.created, 0)) + ' ago)">' + created.date + ' <span>' + created.time + '</span></span>';
+
+		if (item.coords) {
+			contentTiles += ' <a href="https://better-location.palider.cz/' + item.coords + '" class="location" target="_blank" title="Open coordinates ' + item.coords + ' in Better Location"><i class="fa fa-map-marker"></i></a>';
+		}
+		if (item.distance) {
+			contentTiles += ' <span class="distance">' + formatDistance(item.distance) + '</span>';
 		}
 		if (item.size !== null) {
 			contentTiles += ' <span class="size">' + formatBytes(item.size, 2) + '</span>';
 		}
-		if (item.width && item.height) {
-			contentTiles += ' <span class="resolution">' + item.width + ' x ' + item.height + '</span>';
-		}
-		if (item.coords) {
-			// @HACK Non-break space is necesssary to proper vertical alignment of the icon
-			contentTiles += ' <a href="https://better-location.palider.cz/' + item.coords + '" class="location" target="_blank" title="Open coordinates ' + item.coords + ' in Better Location">&nbsp;<i class="fa fa-map-marker"></i></a>';
-		}
-		if (item.distance) {
-			contentTiles += ' <span class="distance">' + formatDistance(item.distance) + '</span>';
+		if (item.created) {
+			const created = item.created.human(true);
+			contentTiles += ' <span class="created" title="' + created + ' (' + msToHuman(Math.max(new Date().getTime() - item.created, 0)) + ' ago)">' + created.date + ' <span>' + created.time + '</span></span>';
 		}
 		contentTiles += '</span>';
 	});
