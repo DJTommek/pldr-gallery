7c1a538da881db875bbe02040c79e572bae4c12e
Tomas Palider (DJTommek) <tomas@palider.cz>
2025-01-10 17:44:06 +0100

    Thumbnails: added support for pre-generating thumbnails for videos

--- src/libs/thumbnailGenerator.js
+++ src/libs/thumbnailGenerator.js
@@ -4,6 +4,9 @@ const pathCustom = require('./path.js');
 const structureRepository = require('./repository/structure.js');
 const cacheHelper = require("../webserver/pages/api/helpers/cache");
 const sharp = require("sharp");
+const ffmpeg = require("fluent-ffmpeg");
+const path = require("path");
+const FS = require("fs");
 
 let generatorRunning = false;
 
@@ -11,11 +14,11 @@ async function generateAllThumbnails() {
 	let counter = 0;
 
 	if (generatorRunning === true) {
-		LOG.info('[Image thumbnail generator] Already running...');
+		LOG.info('[Thumbnail generator] Already running...');
 		return;
 	}
 	generatorRunning = true;
-	LOG.info('[Image thumbnail generator] Starting generating thumbnails...');
+	LOG.info('[Thumbnail generator] Starting generating thumbnails...');
 	const generatingStart = process.hrtime();
 
 	try {
@@ -33,7 +36,7 @@ async function generateAllThumbnails() {
 		}
 	} finally {
 		let generatingDoneTime = msToHuman(hrtime(process.hrtime(generatingStart)));
-		LOG.info('[Image thumbnail generator] Generated ' + counter + ' thumbnails in ' + generatingDoneTime + '.');
+		LOG.info('[Thumbnail generator] Generated ' + counter + ' thumbnails in ' + generatingDoneTime + '.');
 		generatorRunning = false;
 	}
 }
@@ -45,34 +48,97 @@ module.exports.generateThumbnails = generateAllThumbnails;
  * @return {Promise<true|null>} Returns true if thumbnail already exists or it was just generated and saved
  */
 async function generateThumbnail(pathItem) {
-	const cacheFilePath = cacheHelper.getPath(cacheHelper.TYPE.IMAGE, pathItem.path, true);
-	if (cacheFilePath !== null) {
-		return null; // Thumbnail already exists
-	}
-
 	if (pathItem.isImage) {
-		await generateImageThumbnail(pathItem);
-		return true;
+		return await generateImageThumbnail(pathItem);
+	} else if (pathItem.isVideo) {
+		return await generateVideoThumbnail(pathItem);
 	}
 
 	return null;
 }
 
+module.exports.generateThumbnail = generateThumbnail;
+
 /**
- *
- * @param pathItem
+ * @param {FileItem} fileItem
  * @return {Promise<boolean>}
  */
-async function generateImageThumbnail(pathItem) {
-	LOG.debug('[Image thumbnail generator] Generating thumbnail for "' + pathItem.path + '"...');
-	const fileAbsolutePath = pathCustom.relativeToAbsolute(pathItem.path, CONFIG.path);
+async function generateImageThumbnail(fileItem) {
+	const cacheFilePath = cacheHelper.getPath(cacheHelper.TYPE.IMAGE, fileItem.path, true);
+	if (cacheFilePath !== null) {
+		return null; // Thumbnail already exists
+	}
+
+	LOG.debug('[Thumbnail generator] Generating thumbnail for image "' + fileItem.path + '"...');
+	const generatingStart = process.hrtime();
+	const fileAbsolutePath = pathCustom.relativeToAbsolute(fileItem.path, CONFIG.path);
 	let compressData = Object.assign({}, CONFIG.compress);
 	compressData.width = CONFIG.thumbnails.width;
 	compressData.height = CONFIG.thumbnails.height;
 	compressData.fit = CONFIG.thumbnails.image.fit;
 	const imageStreamSharp = await sharp(fileAbsolutePath).withMetadata().resize(compressData);
-	await cacheHelper.saveStream(cacheHelper.TYPE.IMAGE, pathItem.path, imageStreamSharp);
-	LOG.debug('[Image thumbnail generator] Generated thumbnail for "' + pathItem.path + '".');
+	await cacheHelper.saveStream(cacheHelper.TYPE.IMAGE, fileItem.path, imageStreamSharp);
+	let generatingDoneTime = msToHuman(hrtime(process.hrtime(generatingStart)));
+	LOG.debug('[Thumbnail generator] Generated thumbnail for image "' + fileItem.path + '" in ' + generatingDoneTime + '.');
+	return true;
 }
 
-module.exports.generateThumbnail = generateThumbnail;
+/**
+ * @param {FileItem} fileItem
+ * @return {Promise<boolean>}
+ */
+async function generateVideoThumbnail(fileItem) {
+	const cacheFilePath = cacheHelper.getPath(cacheHelper.TYPE.VIDEO, fileItem.path, false);
+	if (FS.existsSync(cacheFilePath) === true) {
+		return null; // Thumbnail already exists
+	}
+
+	LOG.debug('[Thumbnail generator] Generating thumbnail for video "' + fileItem.path + '"...');
+	const generatingStart = process.hrtime();
+	const fileAbsolutePath = pathCustom.relativeToAbsolute(fileItem.path, CONFIG.path);
+
+	const cacheFileDir = path.dirname(cacheFilePath)
+	FS.mkdirSync(cacheFileDir, {recursive: true});
+
+	const errorPrefix = '[Thumbnail generator] Unable to generate thumbnail for video "' + fileItem.path + '": ';
+
+	const ffmpegResult = await new Promise(function (resolve, reject) {
+		ffmpeg(fileAbsolutePath)
+			.screenshots({
+				count: 1, // 1 = thumbnail from middle of video
+				folder: cacheFileDir,
+				filename: path.basename(cacheFilePath),
+				size: CONFIG.thumbnails.width + 'x' + CONFIG.thumbnails.height,
+			})
+			.autopad()
+			.on('error', function (error) {
+				if (error.message.includes('moov atom not found')) {
+					LOG.warning(errorPrefix + '"moov atom not found". File seems to be corrupted, is it still playable?');
+				} else {
+					LOG.error(errorPrefix + error);
+				}
+				reject(error);
+			})
+			.on('end', function () {
+				resolve(true);
+			});
+	});
+
+	let generatingDoneTime = msToHuman(hrtime(process.hrtime(generatingStart)));
+
+	// Check that thumbnail was really generated
+	let thumbnailFileStats;
+	try {
+		thumbnailFileStats = FS.statSync(cacheFilePath);
+	} catch (error) {
+		throw new Error('[Thumbnail generator] Generating thumbnail for video "' + fileItem.path + '" was finished in ' + generatingDoneTime + ' but error occured while thumbnail file check: "' + error.message + '"');
+	}
+
+	if (thumbnailFileStats.size < 128) { // Minimum size in bytes
+		throw new Error('[Thumbnail generator] Generating thumbnail for video "' + fileItem.path + '" was finished in ' + generatingDoneTime + ' but thumbnail size ' + formatBytes(thumbnailFileStats.size) + ' is too small.');
+	}
+
+	LOG.debug('[Thumbnail generator] Generated thumbnail for video "' + fileItem.path + '" in ' + generatingDoneTime + '.');
+	return ffmpegResult;
+}
+
