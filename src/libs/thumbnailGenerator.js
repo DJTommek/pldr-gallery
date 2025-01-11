d551c734fe004305dc430bcdf6e7e82e9578cecf
Tomas Palider (DJTommek) <tomas@palider.cz>
2025-01-11 12:55:05 +0100

    Thumbnail generator: optimized to run fast-checks first, checking if thumbnail file is good, greatly refactored

--- src/libs/thumbnailGenerator.js
+++ src/libs/thumbnailGenerator.js
@@ -26,7 +26,7 @@ async function generateAllThumbnails() {
 		for await (const row of rowsStream) {
 			const pathItem = structureRepository.rowToItem(row);
 			try {
-				const result = await this.generateThumbnail(pathItem)
+				const result = await generateThumbnail(pathItem)
 				if (result === true) {
 					counter++;
 				}
@@ -48,37 +48,59 @@ module.exports.generateThumbnails = generateAllThumbnails;
  * @return {Promise<true|null>} Returns true if thumbnail already exists or it was just generated and saved
  */
 async function generateThumbnail(pathItem) {
-	if (pathItem.isImage) {
-		return await generateImageThumbnail(pathItem);
-	} else if (pathItem.isVideo) {
-		return await generateVideoThumbnail(pathItem);
-	} else if (pathItem.isFolder) {
-		return await generateDirectoryThumbnail(pathItem);
+	if (shouldBeIgnored(pathItem)) {
+		return null;
 	}
 
-	return null;
-}
+	const cacheItemType = cacheHelper.getTypeFromItem(pathItem)
+	if (cacheItemType === null || cacheHelper.exists(cacheItemType, pathItem.path)) {
+		return null; // Thumbnail should not be generated or already exists
+	}
 
-module.exports.generateThumbnail = generateThumbnail;
+	const generatingStart = process.hrtime();
+	let result = null;
+
+	if (pathItem.isImage) {
+		LOG.debug('[Thumbnail generator] Generating thumbnail for "' + pathItem.path + '"...');
+		result = await generateImageThumbnail(pathItem);
+	} else if (pathItem.isVideo) {
+		LOG.debug('[Thumbnail generator] Generating thumbnail for "' + pathItem.path + '"...');
+		result = await generateVideoThumbnail(pathItem);
+	} else if (pathItem.isFolder) {
+		LOG.debug('[Thumbnail generator] Generating thumbnail for "' + pathItem.path + '"...');
+		result = await generateDirectoryThumbnail(pathItem);
+	}
+
+	if (result === null) {
+		return null;
+	}
+
+	let generatingDoneTime = msToHuman(hrtime(process.hrtime(generatingStart)));
+	// Validate that thumbnail was really generated
+	const cacheFilePath = cacheHelper.getPath(cacheHelper.getTypeFromItem(pathItem), pathItem.path, true);
+	let thumbnailFileStats;
+	const message = 'Generating thumbnail for "' + pathItem.path + '" was finished in ' + generatingDoneTime;
+
+	try {
+		thumbnailFileStats = FS.statSync(cacheFilePath);
+	} catch (error) {
+		throw new Error(message + ' but error occured while thumbnail file check: "' + error.message + '"');
+	}
+
+	if (thumbnailFileStats.size < 128) { // Minimum size in bytes
+		throw new Error(message + ' but thumbnail size ' + formatBytes(thumbnailFileStats.size) + ' is too small.');
+	}
+
+	LOG.debug('[Thumbnail generator] ' + message + '.');
+
+	return result;
+}
 
 /**
  * @param {FileItem} fileItem
  * @return {Promise<boolean>}
  */
 async function generateImageThumbnail(fileItem) {
-	for(const ignorePath of CONFIG.thumbnails.image.ignore) {
-		if (fileItem.path.startsWith(ignorePath)) {
-			return null
-		}
-	}
-
-	const cacheFilePath = cacheHelper.getPath(cacheHelper.TYPE.IMAGE, fileItem.path, true);
-	if (cacheFilePath !== null) {
-		return null; // Thumbnail already exists
-	}
-
-	LOG.debug('[Thumbnail generator] Generating thumbnail for image "' + fileItem.path + '"...');
-	const generatingStart = process.hrtime();
 	const fileAbsolutePath = pathCustom.relativeToAbsolute(fileItem.path, CONFIG.path);
 	let compressData = Object.assign({}, CONFIG.compress);
 	compressData.width = CONFIG.thumbnails.width;
@@ -86,8 +108,6 @@ async function generateImageThumbnail(fileItem) {
 	compressData.fit = CONFIG.thumbnails.image.fit;
 	const imageStreamSharp = await sharp(fileAbsolutePath).withMetadata().resize(compressData);
 	await cacheHelper.saveStream(cacheHelper.TYPE.IMAGE, fileItem.path, imageStreamSharp);
-	let generatingDoneTime = msToHuman(hrtime(process.hrtime(generatingStart)));
-	LOG.debug('[Thumbnail generator] Generated thumbnail for image "' + fileItem.path + '" in ' + generatingDoneTime + '.');
 	return true;
 }
 
@@ -96,27 +116,17 @@ async function generateImageThumbnail(fileItem) {
  * @return {Promise<boolean>}
  */
 async function generateVideoThumbnail(fileItem) {
-	for(const ignorePath of CONFIG.thumbnails.video.ignore) {
-		if (fileItem.path.startsWith(ignorePath)) {
-			return null
-		}
-	}
+	LOG.debug('[Thumbnail generator] Generating thumbnail for video "' + fileItem.path + '"...');
 
 	const cacheFilePath = cacheHelper.getPath(cacheHelper.TYPE.VIDEO, fileItem.path, false);
-	if (FS.existsSync(cacheFilePath) === true) {
-		return null; // Thumbnail already exists
-	}
-
-	LOG.debug('[Thumbnail generator] Generating thumbnail for video "' + fileItem.path + '"...');
-	const generatingStart = process.hrtime();
+	const cacheFileDir = path.dirname(cacheFilePath)
 	const fileAbsolutePath = pathCustom.relativeToAbsolute(fileItem.path, CONFIG.path);
 
-	const cacheFileDir = path.dirname(cacheFilePath)
 	FS.mkdirSync(cacheFileDir, {recursive: true});
 
 	const errorPrefix = '[Thumbnail generator] Unable to generate thumbnail for video "' + fileItem.path + '": ';
 
-	const ffmpegResult = await new Promise(function (resolve, reject) {
+	return await new Promise(function (resolve, reject) {
 		ffmpeg(fileAbsolutePath)
 			.screenshots({
 				count: 1, // 1 = thumbnail from middle of video
@@ -137,45 +147,18 @@ async function generateVideoThumbnail(fileItem) {
 				resolve(true);
 			});
 	});
-
-	let generatingDoneTime = msToHuman(hrtime(process.hrtime(generatingStart)));
-
-	// Check that thumbnail was really generated
-	let thumbnailFileStats;
-	try {
-		thumbnailFileStats = FS.statSync(cacheFilePath);
-	} catch (error) {
-		throw new Error('[Thumbnail generator] Generating thumbnail for video "' + fileItem.path + '" was finished in ' + generatingDoneTime + ' but error occured while thumbnail file check: "' + error.message + '"');
-	}
-
-	if (thumbnailFileStats.size < 128) { // Minimum size in bytes
-		throw new Error('[Thumbnail generator] Generating thumbnail for video "' + fileItem.path + '" was finished in ' + generatingDoneTime + ' but thumbnail size ' + formatBytes(thumbnailFileStats.size) + ' is too small.');
-	}
-
-	LOG.debug('[Thumbnail generator] Generated thumbnail for video "' + fileItem.path + '" in ' + generatingDoneTime + '.');
-	return ffmpegResult;
 }
 
 /**
+ * Generate thumbnail for directory by loading random files anywhere in that directory and composite them together in
+ * one image.
+ *
  * @param {FolderItem} directoryItem
  * @return {Promise<boolean>}
  */
 async function generateDirectoryThumbnail(directoryItem) {
-	for(const ignorePath of CONFIG.thumbnails.folder.ignore) {
-		if (directoryItem.path.startsWith(ignorePath)) {
-			return null
-		}
-	}
 
-	const cacheFilePath = cacheHelper.getPath(cacheHelper.TYPE.FOLDER, directoryItem.path, true);
-	if (cacheFilePath !== null) {
-		return null; // Thumbnail already exists
-	}
-
-	LOG.debug('[Thumbnail generator] Generating thumbnail for directory "' + directoryItem.path + '"...');
-	const generatingStart = process.hrtime();
-
-	// get random X images and generate promises for final composing
+	// Get random X images and generate promises for final composing
 	const requiredFilesCount = CONFIG.thumbnails.folder.positions.length;
 	const randomFiles = await structureRepository.randomFiles2(directoryItem.path, {count: requiredFilesCount, onlyImages: true});
 	if (randomFiles.length !== requiredFilesCount) {
@@ -192,9 +175,9 @@ async function generateDirectoryThumbnail(directoryItem) {
 		resizePromises.push(sharpPromise);
 	});
 
-	// resize all loaded images and use them to build final thumbnail
-	const result = await Promise.all(resizePromises).then(async function (resizedImages) {
-		let toComposite = [];
+	// Resize all loaded images and use them to build final thumbnail
+	return await Promise.all(resizePromises).then(async function (resizedImages) {
+		const toComposite = [];
 		resizedImages.forEach(function (resizedImage, index) {
 			toComposite.push({input: resizedImages[index], gravity: CONFIG.thumbnails.folder.positions[index].gravity})
 		});
@@ -204,12 +187,23 @@ async function generateDirectoryThumbnail(directoryItem) {
 
 		await cacheHelper.saveStream(cacheHelper.TYPE.FOLDER, directoryItem.path, thumbnailImageStream);
 		return true;
-	}).catch(function (error) {
-		LOG.error('[Thumbnail generator] Error while generating thumbnail image for directory "' + directoryItem.path + '": ' + error.message);
-		throw error;
 	});
+}
 
-	let generatingDoneTime = msToHuman(hrtime(process.hrtime(generatingStart)));
-	LOG.debug('[Thumbnail generator] Generated thumbnail for directory "' + directoryItem.path + '" in ' + generatingDoneTime + '.');
-	return result;
+function shouldBeIgnored(pathItem) {
+	let ignorePaths = [];
+	if (pathItem.isImage) {
+		ignorePaths = CONFIG.thumbnails.image.ignore;
+	} else if (pathItem.isVideo) {
+		ignorePaths = CONFIG.thumbnails.video.ignore;
+	} else if (pathItem.isFolder) {
+		ignorePaths = CONFIG.thumbnails.folder.ignore;
+	}
+
+	for (const ignorePath of ignorePaths) {
+		if (pathItem.path.startsWith(ignorePath)) {
+			return true;
+		}
+	}
+	return false;
 }
