38036332a5a432d866744bdf1afddaa9044a5326
Tomas Palider (DJTommek) <tomas@palider.cz>
2025-01-14 20:59:28 +0100

    API search: removed sorting from COUNT query

--- src/webserver/pages/api/search.js
+++ src/webserver/pages/api/search.js
@@ -58,6 +58,7 @@ module.exports = function (webserver, endpoint) {
 			const searchQueryTotal = await searchQuery
 				.clone()
 				.clearSelect()
+				.clearOrder()
 				.count('id as totalCount')
 				.first();
 			const rowsTotalCount = searchQueryTotal['totalCount'];
