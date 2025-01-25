64242ac5fec4377d766b3647188e50c9fadeb609
Tomas Palider (DJTommek) <tomas@palider.cz>
2025-01-25 09:51:38 +0100

    API archive: - rewritten to use data only from database instead of full directory scanning - refactored to use `res.locals.pathItem` - removed unused methods in `getItemsFromFolder.js` - removed unused library `globby`

--- src/webserver/pages/api/archive.js
+++ src/webserver/pages/api/archive.js
@@ -1,11 +1,9 @@
-const c = require(BASE_DIR_GET('/src/libs/config.js'));
-const FS = require('fs');
+const CONFIG = require(BASE_DIR_GET('/src/libs/config.js'));
 const Archiver = require('archiver');
 const LOG = require(BASE_DIR_GET('/src/libs/log.js'));
-const getItemsHelper = require(__dirname + '/helpers/getItemsFromFolder.js');
+const structureRepository = require('../../../libs/repository/structure');
 
 module.exports = function (webserver, endpoint) {
-
 	/**
 	 * Directly stream archived file(s) and folder(s) as ZIP file without saving to file.
 	 *
@@ -13,42 +11,58 @@ module.exports = function (webserver, endpoint) {
 	 * @returns streamed file if ok
 	 * @returns JSON if error
 	 */
-	webserver.get(endpoint, function (req, res) {
-		if (!res.locals.fullPathFolder) {
+	webserver.get(endpoint, async function (req, res) {
+		if (CONFIG.archive.enabled !== true) {
+			return res.result.setError('Downloading directory as archive is disabled.').end(400);
+		}
+
+		/** @var {FolderItem|null} */
+		const directoryItem = res.locals.pathItem;
+		if (directoryItem?.isFolder !== true) {
 			return res.result.setError('Invalid path or you dont have a permission.').end(403);
 		}
-		if (c.archive.enabled !== true) {
-			return res.result.setError('Generating archive files are disabled in server config. Check out "config.archive.enabled".').end(403);
-		}
-		// name generated from folder, which is being archived. For root it is default name
-		const zipFile = encodeURI((res.locals.path.split('/').last(2) || 'archive') + '.' + c.archive.format);
-		getItemsHelper.files(res.locals.path, res.locals.fullPathFolder, res.locals.user.getPermissions(), {recursive: true}).then(function (data) {
-			try {
-				const archive = Archiver(c.archive.format, c.archive.options);
-				archive.on('error', function (error) {
-					LOG.error('(Web) Archiver error while generating archive file for path "' + res.locals.path + '": ' + error.message);
-					return res.result.setError('Archiver error while generating archive file.').end(500);
-				});
-				archive.on('warning', function (error) {
-					LOG.error('(Web) Archiver warning while generating archive file for path "' + res.locals.path + '": ' + error.message);
-				});
-				archive.pipe(res);
-				for (const item of data.items) {
-					const pathInArchive = item.path.replace(res.locals.path, '');
-					if (item.isFile) {
-						archive.file(c.path + item.path, {name: pathInArchive});
-					}
+
+		try {
+			const searchOptions = {
+				type: structureRepository.TYPE_FILE
+			};
+			const searchQuery = structureRepository.search(directoryItem.path, searchOptions);
+			searchQuery.andWhere(function () {
+				for (const permission of res.locals.user.getPermissions()) {
+					// 'orWhereLike()' cannot be used due to bug of forcing COLLATE, which slows down the query
+					// @link https://github.com/knex/knex/issues/5143 whereLike does not work with the MySQL utf8mb4 character set
+					this.orWhere('path', 'LIKE', permission + '%');
 				}
-				res.writeHead(200, {
-					'Content-Type': 'application/zip',
-					'Content-Disposition': 'attachment; filename="' + zipFile + '"'
-				});
-				archive.finalize();
-				LOG.info('(Web) Creating and streaming zip file to download: ' + res.locals.path);
-			} catch (error) {
-				LOG.error('(Web) Error while generating archive file for path "' + res.locals.path + '": ' + error.message);
-				return res.result.setError('Error while generating archive file.').end(500);
+			});
+
+			const archive = Archiver(CONFIG.archive.format, CONFIG.archive.options);
+			archive.on('error', function (error) {
+				LOG.error('(Web) Archiver error while generating archive file for path "' + directoryItem + '": ' + error.message);
+				throw error;
+			});
+			archive.on('warning', function (error) {
+				LOG.error('(Web) Archiver warning while generating archive file for path "' + directoryItem + '": ' + error.message);
+			});
+			archive.pipe(res);
+
+			for await (const row of searchQuery.stream()) {
+				/** @type {FileItem} */
+				const fileItem = structureRepository.rowToItem(row);
+				const pathInArchive = fileItem.path.replace(directoryItem.path, '');
+				archive.file(CONFIG.path + fileItem.path, {name: pathInArchive});
 			}
-		});
+
+			// Name generated from folder, which is being archived.
+			const archiveName = encodeURI((directoryItem.basename || 'archive') + '.' + CONFIG.archive.format);
+			res.writeHead(200, {
+				'Content-Type': 'application/zip',
+				'Content-Disposition': 'attachment; filename="' + archiveName + '"'
+			});
+			await archive.finalize();
+			LOG.info('(Web) Creating and streaming zip file to download: ' + directoryItem);
+		} catch (error) {
+			LOG.error('(Web) Error while generating archive file for path "' + directoryItem + '": ' + error.message);
+			return res.result.setError('Error while generating archive file for directory "' + directoryItem + '", try again later.').end(500);
+		}
 	});
 };
